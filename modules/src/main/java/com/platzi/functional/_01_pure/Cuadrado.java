6ae32a4a319b2725e21368987bb2fc666c1a04ba
Jefferson Orlando Perez Carmona <jefferson.perez@sofka.com.co>
2020-03-11 15:41:37 -0500

    se crean mas funciones

--- modules/src/main/java/com/platzi/functional/_01_pure/Cuadrado.java
+++ modules/src/main/java/com/platzi/functional/_01_pure/Cuadrado.java
@@ -1,6 +1,7 @@
 package com.platzi.functional._01_pure;
 
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class Cuadrado {
     public static void main(String[] args) {
@@ -11,6 +12,38 @@ public class Cuadrado {
                 return x * x;
             }
         };
-        System.out.println(cuadrado.apply(5));;
+        System.out.println(cuadrado.apply(5));
+        ;
+
+
+        Function<Integer, Boolean> isOdd = x -> x % 2 == 1;
+
+        Predicate<Integer> isEven = x -> x % 2 == 0;
+        System.out.println( isEven.test(4));
+
+
+        Predicate<Student> isApproved = student -> student.getCalificacion() >= 6.0;
+
+        Student xd = new Student(5.0);
+
+
+        System.out.println(isApproved.test(xd));
+
     }
+
+    static class Student {
+        private double calificacion;
+
+        public Student(double calificacion) {
+            this.calificacion = calificacion;
+        }
+
+        public double getCalificacion() {
+            return calificacion;
+        }
+    }
+
+
 }
+
+
